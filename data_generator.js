ea9db527cdab05434fe31a50ebeedb91a6c02dc6
Matthew <mcupham@gmail.com>
2017-11-07 19:50:17 -0800

    added show user new tweets, timestamps, filter usernames functions

--- data_generator.js
+++ data_generator.js
@@ -7,10 +7,12 @@
 window.streams = {};
 streams.home = [];
 streams.users = {};
+
 streams.users.shawndrost = [];
 streams.users.sharksforcheap = [];
 streams.users.mracus = [];
 streams.users.douglascalhoun = [];
+
 window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
